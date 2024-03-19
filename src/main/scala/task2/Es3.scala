0488aa2c06051e05609f3ab58d8d2b31f2d0720c
FilippoGurioli <filippo.gurioli@studio.unibo.it>
2024-03-19 18:25:28 +0100

    Put all exercises together to match requirements

--- src/main/scala/task2/Es3.scala
+++ src/main/scala/task2/Es3.scala
@@ -50,3 +50,44 @@ object Task2 extends App:
     
     println(genericCompose[Int, String, Boolean](positive, b =>  b match { case v if v => 1; case _ => -1 })(false)) //negative
     //The only constraint is that the codomain of g must be the domain of f
+
+    //Es 6
+    def gcd(a: Int, b: Int): Int = 
+        val max = Math.max(a, b)
+        val min = Math.min(a, b)
+        (max, min) match
+            case (max, min) if max % min == 0 => min
+            case (max, min) => gcd(min, max % min)
+    //this function should already be tail-recursive due to the fact that when I do the recursion (line 9) no other operations are specified near to it
+    
+    println(gcd(12, 8)) //4
+    println(gcd(14, 7)) //7
+
+    //Es 7
+    enum Shape:
+        case Rectangle(width: Double, height: Double)
+        case Circle(radius: Double)
+        case Square(edge: Double)
+    
+    object Rectangle:
+        def area(rect: Shape.Rectangle) = rect match
+            case Shape.Rectangle(w, h) => w * h
+    
+    object Circle:
+        def area(circle: Shape.Circle) = circle match
+            case Shape.Circle(r) => Math.PI * r * r
+
+    object Square:
+        def area(square: Shape.Square) = square match
+            case Shape.Square(e) => e * e
+
+    object Shape:
+        def perimeter(shape: Shape) = shape match
+            case Shape.Rectangle(w, h) => w * 2 + h * 2
+            case Shape.Circle(r) => 2 * r * Math.PI
+            case Shape.Square(e) => 4 * e
+        
+        def scale(shape: Shape, alpha: Double) = shape match
+            case Shape.Rectangle(w, h) => Shape.Rectangle(w * alpha, h * alpha)
+            case Shape.Circle(r) => Shape.Circle(r * alpha)
+            case Shape.Square(e) => Shape.Square(e * alpha)
