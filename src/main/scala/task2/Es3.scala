f7a26a9684cec7fa4fff67f67f26334b5f557501
FilippoGurioli <filippo.gurioli@studio.unibo.it>
2024-03-23 10:54:58 +0100

    moved everything in a single file

--- src/main/scala/task2/Es3.scala
+++ src/main/scala/task2/Es3.scala
@@ -91,3 +91,81 @@ object Task2 extends App:
             case Shape.Rectangle(w, h) => Shape.Rectangle(w * alpha, h * alpha)
             case Shape.Circle(r) => Shape.Circle(r * alpha)
             case Shape.Square(e) => Shape.Square(e * alpha)
+
+    object Optionals:
+  /**
+   * Optional is a type that represents a value that may or may not be present.
+   * Similar to Optional in Java but using the ADT concept.
+   * Therefore, an Optional is a sum type with two cases: Maybe and Empty.
+   * Maybe contains the value, and Empty represents the absence of a value.
+   *
+   * @tparam A
+   */
+  enum Optional[A]:
+    case Maybe(value: A)
+    case Empty()
+
+  object Optional:
+    /**
+     * isEmpty returns true if the optional is Empty, false otherwise.
+     * Example:
+     *
+     * isEmpty(Empty()) == true
+     * isEmpty(Maybe(1)) == false
+     *
+     * @param optional the optional to check
+     * @tparam A the type of the optional
+     * @return true if the optional is Empty, false otherwise
+     */
+    def isEmpty[A](optional: Optional[A]): Boolean = optional match
+      case Empty() => true
+      case _ => false
+
+    /**
+     *
+     * getOrElse returns the value of the optional if it is Maybe, otherwise it returns the default value.
+     * Example:
+     * orElse(Maybe(1), 0) == 1
+     * orElse(Empty(), 0) == 0
+     *
+     * @param optional the optional to get the value from
+     * @param default the default value to return if the optional is Empty
+     * @tparam A the type of the optional
+     * @tparam B the type of the default value
+     * @return the value of the optional if it is Maybe, otherwise the default value
+     */
+    def orElse[A, B >: A](optional: Optional[A], default: B): B = optional match
+      case Maybe(value) => value
+      case Empty() => default
+
+    /**
+     * map applies the function f to the value of the optional if it is Maybe, otherwise it returns Empty.
+     * Example:
+     *
+     * map(Maybe(1), (x: Int) => x + 1) == Maybe(2)
+     * map(Empty(), (x: Int) => x + 1) == Empty()
+     *
+     *
+     * @param optional the optional to apply the function to
+     * @param f the function to apply to the value of the optional
+     * @tparam A the type of the optional
+     * @tparam B the type of the result of the function
+     * @return the result of applying the function to the value of the optional if it is Maybe, otherwise Empty
+     */
+    def map[A, B](optional: Optional[A], f: A => B): Optional[B] = optional match
+      case Empty() => Optional.Empty()
+      case Maybe(value) => Optional.Maybe(f(value))
+
+    /**
+      * a function that keeps the value (if present, otherwise the output is None) only if it satisfies the given predicate
+      *
+      * @param optional the optional to apply the filter
+      * @param predicate the filter to apply to the value of the optional
+      * @tparam A the type of the optional
+      * @return the inputted optional only when the filter is satisfied and the optional was not empty
+      */
+    def filter[A](optional: Optional[A], predicate: A => Boolean) = optional match
+      case Empty() => Optional.Empty()
+      case Maybe(value) => predicate match
+        case p if p(value) => Optional.Maybe(value)
+        case _ => Optional.Empty()
